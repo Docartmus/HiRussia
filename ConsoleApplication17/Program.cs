d78b4833fed7eebafe790e626c345294737534b2
sashapekhurov <pekhurov@gmail.com>
2015-10-12 17:45:58 +0300

    Мяу

--- ConsoleApplication17/Program.cs
+++ ConsoleApplication17/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication17
         static void Main(string[] args)
         {
             Console.WriteLine("Привет Боря");
-            Console.WriteLine("Саша Пипка");
+            Console.WriteLine("Саша Пипкап");
         }
     }
 }
